19ec8da7157ae1ae4589dbcc39f7aee815c169d8
benpemberton <benedictpemberton@gmail.com>
2021-05-21 17:53:12 +0100

    Can select different markers and place them on board

--- index.js
+++ index.js
@@ -1,23 +1,39 @@
-const gameboard = (() => {
-  const tictacs = ['X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'O'];
-  return {tictacs};
-})();
-
-const displayController = (() => {
-  const populateBoard = () => {
-    let cells = document.querySelectorAll('.grid-cell');
-    let i = 0;
-    cells.forEach(cell => {
-      cell.innerHTML = gameboard.tictacs[i];
-      i++;
-    });
-  }
-  return {populateBoard};
-})();
-
-displayController.populateBoard()
-
 const Player = (name) => {
-  const points = 0;
-  return { name, points }
+  let points = 0;
+  return {name, points}
 }
+
+const game = (() => {
+  let player = 'X';
+  const playerTurn = () => {
+    return player;
+  }
+  const changePlayer = (e) => {
+    if (e.currentTarget.classList.contains('player')) {
+      player = e.currentTarget.textContent;
+    } else {
+      player === 'X'? player = 'O': player = 'X';
+    }
+  }
+  return {playerTurn, changePlayer};
+})();
+
+const gameBoard = (() => {
+  const tictacs = ['X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'O'];
+  const placeMarker = (e) => {
+    if (e.target.innerHTML === '') {
+      e.target.innerHTML = game.playerTurn();
+    }
+  }
+  return {tictacs, placeMarker};
+})();
+
+const cells = document.querySelectorAll('.grid-cell');
+cells.forEach(cell => {
+  cell.addEventListener('click', gameBoard.placeMarker);
+});
+
+const playerBtns = document.querySelectorAll('.player');
+playerBtns.forEach(btn => {
+  btn.addEventListener('click', game.changePlayer);
+});
